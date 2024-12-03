4a45d8b0fd487277ee5d2a43f0a897b7af091389
Veniamin <benjaminshapkin@gmail.com>
2024-12-03 11:08:57 +0200

    week4loops

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int sum = 0;
+
+for(int i = 1; i <= 10; i++) //i-- = i - 1
+
+{
+    Console.WriteLine($"i = {i}"); 
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i; //sum += 1;
+}
+
+//Console.WriteLine($"Final total{sum}");
---
+++ RandomNum/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+int myRandomNum;
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"random sum total: {randomSum}");    
